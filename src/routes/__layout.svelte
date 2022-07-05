7a1b1544d90550834ad50982c73a1c00068607c3
Emre Kayık <emrekayik1905@gmail.com>
2022-07-05 22:12:39 +0300

    svelte and tailwindcss v0.1

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,7 @@
+<script>
+    import "../app.css";
+  </script>
+  
+  <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+    <slot/>
+  </button>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+</svelte:head>
+
+<h1 class="text-4xl font-bold underline">
+    Hello world!
+</h1>
